9f1214d549f01440e4829029dad84431954b5296
Hugo Menezes <hugomenezes@gmail.com>
2018-04-27 14:31:31 -0300

    #2 gitignore

--- Plataforma-SDK.NET.Tests/ONS/PlataformaSDK/ProcessMemory/ProcessMemoryClientTest.cs
+++ Plataforma-SDK.NET.Tests/ONS/PlataformaSDK/ProcessMemory/ProcessMemoryClientTest.cs
@@ -1,25 +1,33 @@
+using Moq;
 using NUnit.Framework;
 using ONS.PlataformaSDK.ProcessMemory;
 using ONS.PlataformaSDK.Http;
-using Moq;
+using ONS.PlataformaSDK.Environment;
 
 namespace ONS.PlataformaSDK.ProcessMemory
 {
     public class ProcessMemoryClientTest
     {
+        private const string PROCESS_INSTANCE_ID = "11de37cb-2a69-45d9-9a46-3b24f57eb25b";
+        private const string URL_HEAD = "http://localhost:9091/11de37cb-2a69-45d9-9a46-3b24f57eb25b/head";
         private ProcessMemoryClient ProcessMemoryClient;
+        private Mock<HttpClient> HttpClientMock;
+        private EnvironmentProperties EnvironmentProperties;
 
         [SetUp]
         public void Setup()
         {
-            var httpClientMock = new Mock<HttpClient>();
-            ProcessMemoryClient = new ProcessMemoryClient(httpClientMock.Object);
+            HttpClientMock = new Mock<HttpClient>();
+            HttpClientMock.Setup(mock => mock.Get(URL_HEAD)).Returns("{head:head}");
+            EnvironmentProperties = new EnvironmentProperties("http", "localhost", "9091");
+            ProcessMemoryClient = new ProcessMemoryClient(HttpClientMock.Object, EnvironmentProperties);
         }
 
         [Test]
         public void Head()
         {   
-            Assert.NotNull(ProcessMemoryClient.Head(""));
+            Assert.AreEqual("{head:head}", ProcessMemoryClient.Head(PROCESS_INSTANCE_ID));
+            HttpClientMock.Verify(httpClient => httpClient.Get(URL_HEAD), Times.Once);
         }
     }
 }

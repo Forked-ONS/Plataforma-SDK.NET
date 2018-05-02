4d4f6057f103b153a40c2cb407ccf34c76ac8355
Hugo Menezes <hugomenezes@gmail.com>
2018-05-02 10:47:45 -0300

    #3 Testes

--- Plataforma-SDK.NET/ONS/PlataformaSDK/Core/CoreClient.cs
+++ Plataforma-SDK.NET/ONS/PlataformaSDK/Core/CoreClient.cs
@@ -1,13 +1,14 @@
 using ONS.PlataformaSDK.Http;
 using ONS.PlataformaSDK.Environment;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace ONS.PlataformaSDK.Core
 {
     public class CoreClient
     {
         private HttpClient HttpClient;
-        private EnvironmentProperties ProcessMemoryEnvironmentProperties;
+        private EnvironmentProperties CoreEnvironmentProperties;
 
         public CoreClient()
         {
@@ -16,9 +17,11 @@ namespace ONS.PlataformaSDK.Core
         public CoreClient(HttpClient httpClient, EnvironmentProperties coreEnvironmentProperties)
         {
             this.HttpClient = httpClient;
-            this.ProcessMemoryEnvironmentProperties = coreEnvironmentProperties;
+            this.CoreEnvironmentProperties = coreEnvironmentProperties;
         }
-        public Operation OperationByProcessId() {
+        public List<Operation> OperationByProcessId(string processId) {
+            var OperationJson = HttpClient.Get($"{CoreEnvironmentProperties.Scheme}://{CoreEnvironmentProperties.Host}:{CoreEnvironmentProperties.Port}" +
+                $"/core/operation?filter=byProcessId&processId={processId}");
             return null;
         }
     }

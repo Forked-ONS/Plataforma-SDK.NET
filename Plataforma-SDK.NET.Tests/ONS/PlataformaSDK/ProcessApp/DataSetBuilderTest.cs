38f13a8ca13a2f9289a3f3d6ab527a8cbfa83262
Hugo Menezes <hugomenezes@gmail.com>
2018-05-09 17:19:55 -0300

    #4 Testes para carregar o dataset

--- Plataforma-SDK.NET.Tests/ONS/PlataformaSDK/ProcessApp/DataSetBuilderTest.cs
+++ Plataforma-SDK.NET.Tests/ONS/PlataformaSDK/ProcessApp/DataSetBuilderTest.cs
@@ -34,15 +34,20 @@ namespace ONS.PlataformaSDK.ProcessApp
         private void AssertFiltroMudancaEstadoOperativo(DataSetBuilder dataSetBuilder)
         {
             EntityFilter FilterEventoOperativo = DataSetBuilder.EntitiesFilters[1];
+            Assert.AreEqual(5, FilterEventoOperativo.Filters.Count);
             Assert.AreEqual("eventomudancaestadooperativo", FilterEventoOperativo.EntityName);
             Assert.AreEqual("menorQueData", FilterEventoOperativo.Filters[0].Name);
             Assert.AreEqual("data_verificada < :data order by data_verificada", FilterEventoOperativo.Filters[0].Query);
+            Assert.True(FilterEventoOperativo.Filters[0].ShouldBeExecuted);
             Assert.AreEqual("maiorQueData", FilterEventoOperativo.Filters[1].Name);
             Assert.AreEqual("data_verificada > :data order by data_verificada", FilterEventoOperativo.Filters[1].Query);
+            Assert.True(FilterEventoOperativo.Filters[1].ShouldBeExecuted);
             Assert.AreEqual("byIntervaloDatas", FilterEventoOperativo.Filters[2].Name);
             Assert.AreEqual("data_verificada >= :dataInicial and data_verificada <= :dataFinal and id_uge in ($idsUges) order by data_verificada", FilterEventoOperativo.Filters[2].Query);
+            Assert.True(FilterEventoOperativo.Filters[2].ShouldBeExecuted);
             Assert.AreEqual("byIdsEventos", FilterEventoOperativo.Filters[3].Name);
             Assert.AreEqual("id_evento in ($idsEventos!)", FilterEventoOperativo.Filters[3].Query);
+            Assert.False(FilterEventoOperativo.Filters[3].ShouldBeExecuted);
             Assert.AreEqual("all", FilterEventoOperativo.Filters[4].Name);
             Assert.Null(FilterEventoOperativo.Filters[4].Query);
             Assert.True(FilterEventoOperativo.Filters[4].ShouldBeExecuted);

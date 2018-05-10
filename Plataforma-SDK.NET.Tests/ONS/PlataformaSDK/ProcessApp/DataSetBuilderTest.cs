b38792bf450d5dc7f8dd7a93156a0d7f1fbcea33
Hugo Menezes <hugomenezes@gmail.com>
2018-05-10 16:43:55 -0300

    #4 Testes para carregar o dataset

--- Plataforma-SDK.NET.Tests/ONS/PlataformaSDK/ProcessApp/DataSetBuilderTest.cs
+++ Plataforma-SDK.NET.Tests/ONS/PlataformaSDK/ProcessApp/DataSetBuilderTest.cs
@@ -7,12 +7,12 @@ namespace ONS.PlataformaSDK.ProcessApp
     public class DataSetBuilderTest
     {
         private const string CONTENT_STR = "unidadegeradora:\n  model: tb_unidade_geradora\n  fields:\n    idUge:\n      column: id_uge\n    potenciaDisponivel:\n      column: pot_disp\n    dataInicioOperacao:\n      column: data_inicio_operacao\n    idUsina:\n      column: id_usina\n  filters:\n    byIdUsina:  \"id_usina in ($idsUsinas)\"\neventomudancaestadooperativo:\n  model: tb_evt_estado_oper\n  fields:\n    idEvento:\n      column: id_evento\n    idUge:\n      column: id_uge\n    idClassificacaoOrigem:\n      column: id_class_origem\n    idEstadoOperativo:\n      column: id_tp_estado_oper\n    idCondicaoOperativa:\n      column: id_cond_oper\n    dataVerificada:\n      column: data_verificada\n    potenciaDisponivel:\n      column: pot_disp\n    numONS: \n      column: numero_ons\n    eversao:\n      column: eversao  \n  filters:\n    menorQueData: \"data_verificada < :data order by data_verificada\"\n    maiorQueData: \"data_verificada > :data order by data_verificada\"\n    byIntervaloDatas: \"data_verificada >= :dataInicial and data_verificada <= :dataFinal and id_uge in ($idsUges) order by data_verificada\"\n    byIdsEventos:  \"id_evento in ($idsEventos!)\"\n    all:";
-        private DataSetBuilder<TesteEntity> DataSetBuilder;
+        private DataSetBuilder DataSetBuilder;
 
         [SetUp]
         public void Setup()
         {
-            DataSetBuilder = new DataSetBuilder<TesteEntity>();
+            DataSetBuilder = new DataSetBuilder();
         }
 
         [Test]
@@ -25,7 +25,7 @@ namespace ONS.PlataformaSDK.ProcessApp
             AssertFiltroMudancaEstadoOperativo(DataSetBuilder);
         }
 
-        private void AssertFiltroUnidadeGeradora(DataSetBuilder<TesteEntity> dataSetBuilder)
+        private void AssertFiltroUnidadeGeradora(DataSetBuilder dataSetBuilder)
         {
             var FilterUnidadeGeradora = DataSetBuilder.EntitiesFilters[0];
             Assert.AreEqual("unidadegeradora", FilterUnidadeGeradora.EntityName);
@@ -33,7 +33,7 @@ namespace ONS.PlataformaSDK.ProcessApp
             Assert.AreEqual("id_usina in ($idsUsinas)", FilterUnidadeGeradora.Filters[0].Query);
         }
 
-        private void AssertFiltroMudancaEstadoOperativo(DataSetBuilder<TesteEntity> dataSetBuilder)
+        private void AssertFiltroMudancaEstadoOperativo(DataSetBuilder dataSetBuilder)
         {
             EntityFilter FilterEventoOperativo = DataSetBuilder.EntitiesFilters[1];
             Assert.AreEqual(5, FilterEventoOperativo.Filters.Count);

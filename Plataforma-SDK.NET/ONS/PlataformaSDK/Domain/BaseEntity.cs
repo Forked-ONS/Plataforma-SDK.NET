cf4a23ac50f4993cf41977802f9ee75b2c4ce7a0
Hugo Menezes <hugomenezes@gmail.com>
2018-05-11 17:28:17 -0300

    #4 Testes para carregar o dataset

---
+++ Plataforma-SDK.NET/ONS/PlataformaSDK/Domain/BaseEntity.cs
@@ -0,0 +1,8 @@
+namespace ONS.PlataformaSDK.Domain
+{
+    public class BaseEntity
+    {
+        public string id{get;set;}
+        public Metadata _metadata{get; set;}
+    }
+}
---
+++ Plataforma-SDK.NET/ONS/PlataformaSDK/Domain/Metadata.cs
@@ -0,0 +1,8 @@
+namespace ONS.PlataformaSDK.Domain
+{
+    public class Metadata
+    {
+        public string Branch{get; set;}
+        public string Type{get; set;}
+    }
+}
